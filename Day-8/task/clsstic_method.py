0b83158aabe7bbedbdb565192135c003821f1978
charan-n-dev <charann539@gmail.com>
2026-02-10 10:32:25 +0530

    new

--- Day-8/task/clsstic_method.py
+++ Day-8/task/clsstic_method.py
@@ -1,5 +1,5 @@
 class Student:
-    college_name = "ABC College"   # class variable
+    college_name = "ABC College"   
 
     def __init__(self, name, roll_no):
         self.name = name
@@ -9,7 +9,6 @@ class Student:
         print(f"Name: {self.name}")
         print(f"Roll No: {self.roll_no}")
         print(f"College: {Student.college_name}")
-        print("-------------------")
 
     @classmethod
     def change_college(cls, new_name):
@@ -23,19 +22,17 @@ class Student:
             return "Fail"
 
 
-# Testing
 s1 = Student("Rahul", 1)
 s2 = Student("Anita", 2)
 
 s1.display()
 s2.display()
 
-# Change college for all students
 Student.change_college("XYZ College")
 
 s1.display()
 s2.display()
 
-# Static method test
+
 print(Student.is_pass(40))
 print(Student.is_pass(20))
